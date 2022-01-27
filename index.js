938ae398488f2fe99df31608a65f9d02be263c95
Timmy <message.timmynguyen@gmail.com>
2022-01-27 12:37:57 -0800

    added more text

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 console.log(`
-timmywashere!
+TIMMYWASHERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+Hello, I am Timmy
 
 I create web landing pages and emails. I am passionate when it comes to front end development, digital marketing, and digital analytics. And, I am thankful to be able to collaborate with some super talented teams to bring amazing thingss to people.
 
+📄 LinkedIn - https://www.linkedin.com/in/nguyen17/
+☕ I spend my afternoons at coffee shops o(〃＾▽＾〃)o
 
+Currently learning nodejs stuff from https://nodecli.com/ (absolutely amazing course by Ahmad Awais!)
 `);
