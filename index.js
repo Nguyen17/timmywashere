fd1bb4bfcb33ea0b1847e51a368238d3744a7a32
Timmy <message.timmynguyen@gmail.com>
2022-01-26 18:45:56 -0800

    added more text

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
-console.log('timmywashere!');
+console.log(`
+timmywashere!
+
+I create web landing pages and emails. I am passionate when it comes to front end development, digital marketing, and digital analytics. And, I am thankful to be able to collaborate with some super talented teams to bring amazing thingss to people.
+
+
+`);
